5580872b7f6c93e3ea3ed1653cb33a55e7d7877b
Heitor <he1tor@hotmail.com>
2020-04-13 19:37:17 -0300

    :recycle: calculator works :construction: calculator functions

--- Calculator/scripts/main.js
+++ Calculator/scripts/main.js
@@ -1,125 +1,141 @@
-const calculator = document.querySelector("body");
-const functionKeyboard = document.querySelectorAll(".main-function-input");
-const numberKeyboard = document.querySelectorAll(".number-input");
-const display = document.querySelector("#display");
-const operator = { 
-    plus: (value1, value2) => value1 + value2,
-    minus: (valie1, value2) => valie1 - value2,
-    times: (value1, value2) => value1 * value2,
-    divded: (value1, value2) => value1 / value2,
-    percent: (value1) => value1 / 100
+let memory1 = '';
+let memory2 = '';
+function Calculator() {
+    const body = document.querySelector("body");
+    const operator = {
+        plus: (value1, value2) => value1 + value2,
+        minus: (valie1, value2) => valie1 - value2,
+        times: (value1, value2) => value1 * value2,
+        divded: (value1, value2) => value1 / value2,
+        percent: (value1) => value1 / 100,
+        AC: () => { },
+        C: () => { },
+    }
+
+    body.addEventListener('keypress', fisicalKeyboard);
+    FunctionKeyboard();
+    NumberKeyboard();
+};
+
+function AddToMemory(value, memory) {
+    if (memory1.length > 7) {
+        return;
+    }
+    if (memory === 1) {
+        if (value === '.') {
+            if (memory1.localeCompare) {
+                memory1 += value
+            }
+
+        } else {
+            if (memory1 === '' && value === '0') {
+                return;
+            }
+            memory1 += value
+        }
+
+        AddNumberOnDisplay(memory1)
+    } else if (memory === 2) {
+        memory2 = memory1;
+
+        memory1 = '';
+    }
 }
-let clear = false
 
-function addNumberOnDisplay(number) {
-    if (Number(display.textContent) === 0 || clear) {
-        display.innerHTML = number;
+function fisicalKeyboard(e) {
+    AddToMemory(e.key);
+}
 
-        clear = false
+function AddNumberOnDisplay(value) {
+    const display = document.querySelector("#display");
+
+    if (!!value) {
+        display.innerHTML = value;
     } else {
-        display.innerHTML += number;
+        display.innerHTML = '0'
+    }
+
+}
+
+
+
+function NumberKeyboard() {
+    const numberKeyboard = document.querySelectorAll(".number-input");
+
+
+    numberKeyboard[0].onclick = () => {
+        AddToMemory(numberKeyboard[0].textContent, 1)
+    }
+
+    numberKeyboard[1].onclick = () => {
+        AddToMemory(numberKeyboard[1].textContent, 1)
+    }
+
+    numberKeyboard[2].onclick = () => {
+        AddToMemory(numberKeyboard[2].textContent, 1)
+    }
+
+    numberKeyboard[3].onclick = () => {
+        AddToMemory(numberKeyboard[3].textContent, 1)
+    }
+
+    numberKeyboard[4].onclick = () => {
+        AddToMemory(numberKeyboard[4].textContent, 1)
+    }
+
+    numberKeyboard[5].onclick = () => {
+        AddToMemory(numberKeyboard[5].textContent, 1)
+    }
+
+    numberKeyboard[6].onclick = () => {
+        AddToMemory(numberKeyboard[6].textContent, 1)
+    }
+
+    numberKeyboard[7].onclick = () => {
+        AddToMemory(numberKeyboard[7].textContent, 1)
+    }
+
+    numberKeyboard[8].onclick = () => {
+        AddToMemory(numberKeyboard[8].textContent, 1)
+    }
+
+    numberKeyboard[9].onclick = () => {
+        AddToMemory(numberKeyboard[9].textContent, 1)
+    }
+
+    numberKeyboard[10].onclick = () => {
+        AddToMemory('.')
     }
 }
 
-function fisicalKey(e) {
-    if (!!Number(e.key) || Number(e.key) === 0) {
-        addNumberOnDisplay(e.key);
+function FunctionKeyboard() {
+    const functionsKeyboard = document.querySelectorAll(".main-function-input");    
+    const table = document.querySelector("tbody");
+
+    table.onclick = () => {
+        if (memory1 != '') {
+            functionsKeyboard[0].innerHTML = 'C'
+        }
     }
+
+    functionsKeyboard[0].onclick = () => {
+
+        if (functionsKeyboard[0].innerHTML === 'C') {
+            memory1 = '';
+
+            functionsKeyboard[0].innerHTML = 'AC'
+            AddNumberOnDisplay(memory1)
+        } else if (functionsKeyboard[0].innerHTML === 'AC') {
+            memory1 = '';
+            memory2 = '';
+
+            functionsKeyboard[0].innerHTML = 'C'
+            // functionsKeyboard
+
+            AddNumberOnDisplay(memory1)
+        }
+    };
 }
 
-calculator.addEventListener('keypress', fisicalKey);
 
-//percent
-functionKeyboard[1].onclick = () => {
-    localStorage.setItem('mainMemory', display.innerHTML);
-    clear = true;
-    addNumberOnDisplay(operator.percent(display.innerHTML));
-}
-
-//divded
-functionKeyboard[2].onclick = () => {
-    localStorage.setItem('mainMemory', display.innerHTML);
-    localStorage.setItem('operator', 'divded')
-    clear = true
-}
-
-//times
-functionKeyboard[3].onclick = () => {
-    localStorage.setItem('mainMemory', display.innerHTML);
-    localStorage.setItem('operator', 'times')
-    clear = true
-}
-
-//minus
-functionKeyboard[4].onclick = () => {
-    localStorage.setItem('mainMemory', display.innerHTML);
-    localStorage.setItem('operator', 'minus')
-    clear = true
-}
-
-//adiction
-functionKeyboard[5].onclick = () => {
-    localStorage.setItem('mainMemory', display.innerHTML);
-    localStorage.setItem('operator', 'plus')
-    clear = true
-}
-
-// iqual
-functionKeyboard[6].onclick = () => {
-    const memory1 = localStorage.getItem('mainMemory')
-    const oper = operator[localStorage.getItem('operator')]
-
-    const answer = oper(Number(memory1), Number(display.innerHTML))
-    clear = true
-    addNumberOnDisplay(answer)
-}
-
-functionKeyboard[0].onclick = () => {
-    clear = true
-    addNumberOnDisplay(0);
-    localStorage.clear();
-}
-
-numberKeyboard[0].onclick = () => {
-    addNumberOnDisplay(numberKeyboard[0].textContent) 
-}
-
-numberKeyboard[1].onclick = () => {
-    addNumberOnDisplay(numberKeyboard[1].textContent)
-}
-
-numberKeyboard[2].onclick = () => {
-    addNumberOnDisplay(numberKeyboard[2].textContent)
-}
-
-numberKeyboard[3].onclick = () => {
-    addNumberOnDisplay(numberKeyboard[3].textContent)
-}
-
-numberKeyboard[4].onclick = () => {
-    addNumberOnDisplay(numberKeyboard[4].textContent)
-}
-
-numberKeyboard[5].onclick = () => {
-    addNumberOnDisplay(numberKeyboard[5].textContent)
-}
-
-numberKeyboard[6].onclick = () => {
-    addNumberOnDisplay(numberKeyboard[6].textContent)
-}
-
-numberKeyboard[7].onclick = () => {
-    addNumberOnDisplay(numberKeyboard[7].textContent)
-}
-
-numberKeyboard[8].onclick = () => {
-    addNumberOnDisplay(numberKeyboard[8].textContent)
-}
-
-numberKeyboard[9].onclick = () => {
-    addNumberOnDisplay(numberKeyboard[9].textContent)
-}
-
-numberKeyboard[10].onclick = () => {
-    addNumberOnDisplay('.')
-}
+Calculator()
